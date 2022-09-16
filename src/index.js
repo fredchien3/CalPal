4d7c87ef35f463d0dbbc3c2a6010eb8ea8adce6c
Frederick <fred.chien3@gmail.com>
2022-09-16 18:01:07 -0400

    setup macro chart and toggling

--- src/index.js
+++ src/index.js
@@ -1,31 +1,9 @@
-import Chart from 'chart.js/auto';
+import { chartUtil } from "./scripts/chartUtil"
 
 document.addEventListener("DOMContentLoaded", () => {
-  const body = document.querySelector('body');
-  // const test = document.createElement('p');
-  // test.innerText = "Hello world!";
-  // body.appendChild(test);
-  const ctx = document.getElementById('macroChart').getContext('2d');
-  const macroData = [12, 19, 3] // protein, fat, carbs
-  const macroPolarChart = new Chart(ctx, {
-    type: 'polarArea',
-    data: {
-        labels: ['Protein', 'Fat', 'Carbohydrates'],
-        datasets: [{
-            label: 'foodProtein',
-            data: macroData,
-            backgroundColor: [
-              'red',
-              'yellow',
-              'blue'
-            ],
-            borderColor: [
-              'red',
-              'yellow',
-              'blue'
-            ],
-            borderWidth: 1
-        }]
-    }
+  chartUtil.setupChart();
+  const toggleMacroChart = document.getElementById('toggleMacroChart');
+  toggleMacroChart.addEventListener('click', () => {
+    chartUtil.toggleChart();
   });
 })
