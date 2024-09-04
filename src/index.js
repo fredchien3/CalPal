15c69a80479334adcafbb6e0f6ff65425a71fb5c
Fred Chien <fred.chien3@gmail.com>
2024-09-04 15:51:54 +0800

    Update welcome modal to only show on first visit

--- src/index.js
+++ src/index.js
@@ -2,14 +2,25 @@ import { CalPalView } from "./scripts/calpal-view"
 
 document.addEventListener("DOMContentLoaded", () => {
   CalPalView.initialize();
-  const aside = document.getElementById('aside');
-  aside.addEventListener('click', (e) => {
-    if (e.currentTarget === e.target) aside.classList.add('hide');
-  });
-  const closeButton = document.getElementById('close-tutorial');
-  closeButton.addEventListener('click', (e) => {
-    aside.classList.add('hide');
-  })
-  document.getElementById('info-button').addEventListener('click', () => aside.classList.remove('hide'));
 
+  const welcomeModal = document.getElementById('aside');
+  welcomeModal.addEventListener('click', (e) => {
+    // If the user clicks on the grayed out portion outside of the modal
+    if (e.currentTarget === e.target) welcomeModal.classList.add('hide');
+  });
+
+  // The modal is hidden by default, being unhidden if it's the user's first visit
+  if (!localStorage.getItem('visited')) {
+    welcomeModal.classList.remove('hide');
+  }
+
+  document.getElementById('close-tutorial').addEventListener('click', () => {
+    welcomeModal.classList.add('hide');
+  })
+
+  document.getElementById('info-button').addEventListener('click', () => {
+    welcomeModal.classList.remove('hide');
+  });
+
+  localStorage.setItem('visited', 1);
 })
